1034fe610b4d3483af16d09ae474e6947f16a6fb
Alexander Sukhovetchenko <alexsukhovetchenko@coherentsolutions.com>
2019-03-15 13:48:43 +0300

    Install dirmngr as one of ansible dependencies

--- bin/install_ansible_debian.sh
+++ bin/install_ansible_debian.sh
@@ -3,7 +3,7 @@
 if ! command -v ansible >/dev/null; then
     echo "Installing Ansible dependencies..."
     sudo apt-get update -qq
-    sudo apt-get install --yes -qq --no-install-recommends software-properties-common
+    sudo apt-get install --yes -qq --no-install-recommends software-properties-common dirmngr
     echo "Adding Ansible PPA repository..."
     echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" >> /etc/apt/sources.list
     sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
